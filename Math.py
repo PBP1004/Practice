ea25af6b51c77494ff4b0760844cd296303dd84b
PBP1004 <priya.p1498@gmail.com>
2024-08-08 14:56:09 -0400

    V1
    N

--- Math.py
+++ Math.py
@@ -2,5 +2,9 @@ x=1
 y=2
 print("Sum of x and y is =")
 print(x+y)
+<<<<<<< Updated upstream
 print("This line is added later")
 print("This is also added later")
+=======
+print("This statment is for undoing tasks")
+>>>>>>> Stashed changes
